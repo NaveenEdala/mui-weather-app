ee543771f309c4dace277d235f50522c752243f7
naveenedala <edala_naveen@srmap.edu.in>
2023-03-09 15:33:22 +0530

    added story variant

--- src/stories/SmallWeatherCard.stories.tsx
+++ src/stories/SmallWeatherCard.stories.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { SmallWeatherCard } from "./SmallWeatherCard";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import ShowerIcon from "@mui/icons-material/Shower";
 
 export default {
-	title: "WeatherApp/Small Weather Card",
+	title: "Weather App/Small Weather Card",
 	component: SmallWeatherCard,
 } as ComponentMeta<typeof SmallWeatherCard>;
 
 const Template: ComponentStory<typeof SmallWeatherCard> = (args) => <SmallWeatherCard {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Sunny = Template.bind({});
+Sunny.args = {
 	weatherCondition: "Sunny",
 	weatherConditionIcon: <WbSunnyIcon />,
 	weatherConditionImage: "https://upload.wikimedia.org/wikipedia/commons/2/26/Sunny_day_in_India.jpg",
@@ -19,4 +20,16 @@ Default.args = {
 	temperature: 30,
 	apparentTemperature: 31.3,
 	timestamp: "2023-11-17T04:00",
+};
+
+export const Rainy = Template.bind({});
+Rainy.args = {
+	weatherCondition: "Rainy",
+	weatherConditionIcon: <ShowerIcon />,
+	weatherConditionImage:
+		"https://upload.wikimedia.org/wikipedia/commons/3/32/GGB_refracts_in_rain_dropletes_original_1.JPG",
+	weatherUnits: "°C",
+	temperature: -2.3,
+	apparentTemperature: 1.7,
+	timestamp: "2023-08-03T15:00",
 };
